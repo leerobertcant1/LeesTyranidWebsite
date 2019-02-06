aa725040e87e2b7a3547319cf983654af877333b
Lee Cant <leerobertcant@gmail.com>
2019-02-06 21:25:11 +0000

    Added a Dependency Inject with CaliburnMicro

--- LeesTyranidWebsite/TyranidsWpfUI/Bootstrapper.cs
+++ LeesTyranidWebsite/TyranidsWpfUI/Bootstrapper.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
 using Caliburn.Micro;
 using TyranidsWpfUI.ViewModels;
 
@@ -6,11 +9,41 @@ namespace TyranidsWpfUI
 {
     class Bootstrapper: BootstrapperBase
     {
+        private SimpleContainer _container = new SimpleContainer();
+
         public Bootstrapper() =>
             Initialize();
-        
+
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            GetContainerTypes();
+        }
+
         protected override void OnStartup(object sender, StartupEventArgs e) =>
             DisplayRootViewFor<ShellViewModel>();
-        
+
+        protected override object GetInstance(Type service, string key) =>
+             _container.GetInstance(service, key);
+
+        protected override IEnumerable<object> GetAllInstances(Type service) =>
+             _container.GetAllInstances(service);
+
+        protected override void BuildUp(object instance) =>
+            _container.BuildUp(instance);
+
+        private void GetContainerTypes()
+        {
+            GetType().Assembly.GetTypes()
+               .Where(t => t.IsClass)
+               .Where(t => t.Name.EndsWith("ViewModel"))
+               .ToList()
+               .ForEach(vmt => _container.RegisterPerRequest(
+                   vmt, vmt.ToString(), vmt));
+        }
     }
 }
