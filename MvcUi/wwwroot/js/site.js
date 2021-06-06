ce60677e3729d6de808eecd80f960dda402646eb
Lee <leerobertcant@gmail.com>
2021-06-06 14:16:12 +0100

    CSS styling done for web page re-sizing. Still need to do mobile.

--- MvcUi/wwwroot/js/site.js
+++ MvcUi/wwwroot/js/site.js
@@ -6,14 +6,52 @@
     }
 
     setNavBarCss(navbarItemId);
+    setImageSize();
+
+    $(window).on("resize", function () {
+        setImageSize();
+    });
 });
 
 function setImageSize() {
+    if (window.location.href.indexOf("Models?") == -1) {
+        return;
+    }
 
+    let imageItems = $(".img-item");
+    let imageRow = $(".img-row");
+    let limits = { Mobile: 600, OnePictureLarge: 1000, OnePictureSmall: 600 };
+
+    for (let rowIndex = 0; rowIndex < imageRow.length; rowIndex++) {
+        for (let itemIndex = 0; itemIndex < imageItems.length; itemIndex++) {
+            let isMobile = imageRow[rowIndex].clientWidth < limits.Mobile;
+
+            imageItems[itemIndex].width = isMobile ? 400 : 500;
+            imageItems[itemIndex].height = isMobile ? 400 : 600;
+
+            setImgCss({ ImageRow: imageRow, RowIndex: rowIndex, ImageItems: imageItems, ItemIndex: itemIndex, Limits: limits });
+
+            console.log(`${imageRow[rowIndex].clientWidth}`);
+        }
+    }
 }
 
-function setNavBarCss(navbarItemId)
-{
+function setImgCss(values) {
+    if (values.ImageRow[values.RowIndex].clientWidth < values.Limits.OnePictureLarge && values.ImageRow[values.RowIndex].clientWidth > values.Limits.OnePictureSmall) {
+        $(values.ImageItems[values.ItemIndex]).addClass("left-margin-large");
+        $(values.ImageItems[values.ItemIndex]).removeClass("left-margin-small");
+        $(".model-role-title", ".tyranids-title").removeClass("left-margin-large");
+        $(".model-role-title", ".tyranids-title").removeClass("left-margin-small");
+    } else if (values.ImageRow[values.RowIndex].clientWidth < values.Limits.OnePictureLarge) {
+        $(values.ImageItems[values.ItemIndex]).removeClass("left-margin-large");
+        $(values.ImageItems[values.ItemIndex], ".model-role-title", ".tyranids-title").addClass("left-margin-small");
+    } else {
+        $(values.ImageItems[values.ItemIndex], ".model-role-title", ".tyranids-title").removeClass("left-margin-large");
+        $(values.ImageItems[values.ItemIndex], ".model-role-title", ".tyranids-title").removeClass("left-margin-small");
+    }
+}
+
+function setNavBarCss(navbarItemId) {
     removeNavbarHighlight();
 
     var isNavBarHome = $.trim(("#" + navbarItemId)).toLowerCase() === "navbarhome";
@@ -28,8 +66,7 @@ function setNavBarCss(navbarItemId)
     sessionStorage.setItem(globals.properties.ids.navbar, navbarItemId);
 }
 
-function removeNavbarHighlight()
-{
+function removeNavbarHighlight(){
     var navbarItems = $(".navbar-item");
 
     if (!navbarItems) {
